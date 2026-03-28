b6b39ee25a89d5f618db1cf9f47065108d2f7b0c
Nikhil Anand <nikhildfire8102@gmail.com>
2026-03-28 18:15:23 +0530

    update

--- backend/src/controllers/notesController.js
+++ backend/src/controllers/notesController.js
@@ -1,11 +1,22 @@
 import Note from "../models/Note.js"; 
 
-export async function getAllNotes(req, res) {
+export async function getAllNotes(_, res) {
   try {
-    const notes = await Note.find();
+    const notes = await Note.find().sort({ createdAt: -1 });
     res.status(200).json(notes);
   } catch (error) {
     console.error("Error in getAllNotes controller:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+}
+
+export async function getNoteById(req, res) {
+  try {
+    const note = await Note.findById(req.params.id);
+    if (!note) return res.status(404).json({ message: "Note not found" });
+    res.status(200).json(note);
+  } catch (error) {
+    console.error("Error in getNoteById controller:", error);
     res.status(500).json({ message: "Internal server error" });
   }
 }
--- backend/src/routes/notesRoutes.js
+++ backend/src/routes/notesRoutes.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { createNote, getAllNotes, updateNote, deleteNote } from "../controllers/notesController.js";
+import { createNote, getAllNotes, updateNote, deleteNote , getNoteById} from "../controllers/notesController.js";
 const router = express.Router();
 
 router.get("/", getAllNotes);
+router.get("/:id", getNoteById);
 router.post("/", createNote);
 router.put("/:id", updateNote);
 router.delete("/:id", deleteNote);
