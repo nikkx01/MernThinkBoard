2869d014f254676392bc783033fbfb9a4048799f
Nikhil Anand <nikhildfire8102@gmail.com>
2026-03-30 17:03:32 +0530

    update

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
+import rateLimiter from "./middleware/rateLimiter.js";
 
 dotenv.config();
 
@@ -13,6 +14,12 @@ const PORT = process.env.PORT || 5001;
 connectDB();
 
 app.use(express.json());
+app.use(rateLimiter);
+
+app.use((req, res, next) => {
+  console.log(`Req method is ${req.method} & Req URL is ${req.url}`);
+  next();
+});
 
 app.use("/api/notes", notesRoutes);
 
