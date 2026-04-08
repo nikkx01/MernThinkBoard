c3191cd6abc4b277a7240ff09af353b8d62de80c
Nikhil Anand <nikhildfire8102@gmail.com>
2026-04-08 15:29:57 +0530

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,10 +3,11 @@ import { Routes, Route } from 'react-router';
 import HomePage from './pages/HomePage';
 import CreatePage from './pages/CreatePage';
 import NoteDetailPage from './pages/NoteDetailPage';  
+import toast from 'react-hot-toast';
 
 const App = () => {
   return <div>
-
+    <button onClick={() => toast.success("congrats")}>Click me</button>
     <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='/create' element={<CreatePage />} />
