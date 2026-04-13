6ef7a9cc51ef83043f6c0bad9860353e5d686fb6
Nikhil Anand <nikhildfire8102@gmail.com>
2026-04-13 17:44:23 +0530

    update

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,6 +4,8 @@ import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
 import rateLimiter from "./middleware/rateLimiter.js";
 
+import cors from "cors";
+
 dotenv.config();
 
 console.log(process.env.MONGO_URI);
@@ -11,13 +13,17 @@ console.log(process.env.MONGO_URI);
 const app = express();
 const PORT = process.env.PORT || 5001;
 
+app.use(cors({
+  origin: "http://localhost:5173",
+}));
+
 app.use(express.json());
 app.use(rateLimiter);
 
-app.use((req, res, next) => {
-  console.log(`Req method is ${req.method} & Req URL is ${req.url}`);
-  next();
-});
+// app.use((req, res, next) => {
+//   console.log(`Req method is ${req.method} & Req URL is ${req.url}`);
+//   next();
+// });
 
 app.use("/api/notes", notesRoutes);
 
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,9 +1,42 @@
+import { useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
+import RateLimitedUI from "../components/RateLimitedUI";
+import axios from "axios";
+import toast from "react-hot-toast";
 
 const HomePage = () => {
+  const [isRateLimited, setIsRateLimited] = useState(true);
+  const [notes, setNotes] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchNotes = async () => {
+      try {
+        const res = await axios.get("http://localhost:5001/api/notes");
+        console.log(res.data);
+        setNotes(res.data);
+        setIsRateLimited(false);
+      } catch (error) {
+        console.error("Error fetching notes:");
+        console.error(error);
+        if(error.response?.status === 429) {
+          setIsRateLimited(true);
+        }else {
+          toast.error("Failed to load notes")
+        }
+      }finally {
+        setLoading(false);
+      }
+    };
+
+    fetchNotes();
+  }, []);
+
   return (
     <div className="min-h-screen">
       <Navbar />
+
+      {isRateLimited && <RateLimitedUI />}
     </div>
   );
 };
